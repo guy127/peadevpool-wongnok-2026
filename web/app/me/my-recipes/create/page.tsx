f65a78e7fe16bd3004f276d0ecdc93cd5bc2847d
Joey Wasin <wasin.ww1402@gmail.com>
2026-09-12 09:56:46 +0700

    feat: add recipe creation page

---
+++ web/app/me/my-recipes/create/page.tsx
@@ -0,0 +1,9 @@
+const RecipeCreationPage = () => {
+  return (
+    <div>
+      <h1>Create Recipe</h1>
+    </div>
+  );
+};
+
+export default RecipeCreationPage;
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/recipes");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
